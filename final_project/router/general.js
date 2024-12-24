e5e07cb166422da2bd7f2785b36e5fc57e5b3995
ingmsm <76408726+ingmsm@users.noreply.github.com>
2024-12-23 23:40:17 -0400

    review general.js
    ahora es posible conocer la review con solo el indice del libro

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -64,8 +64,10 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+    let review= books[isbn]
+    let reviews = review.reviews
+  return res.send(reviews);
 });
 
 module.exports.general = public_users;
